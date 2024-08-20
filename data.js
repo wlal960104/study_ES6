59c82f74be2f3799938205dae80ef916d5a29e8e
jimi <jimi96@softworks.co.kr>
2024-08-20 18:56:44 +0900

    [24.08.20] mls parsing 저장용 커밋

---
+++ data.js
@@ -0,0 +1,64 @@
+export default
+{
+        "status": 200,
+        "statusText": "OK",
+        "data": {
+            "totalCnt": 3,
+            "prdList": [
+                {
+                    "prdId": "PR00000274",
+                    "prdNm": "해피독TV",
+                    "prdBnfNm": "해피독TV 이용권",
+                    "prdImg": "FOBO/PRD/PRDDSP/20240110/thumb_해피독TV.png",
+                    "prdFprcAmt": "0",
+                    "prdSprcAmt": "8360",
+                    "discntRate": "0",
+                    "scbPayTrmCd": "03",
+                    "sllStCd": "02",
+                    "likeYn": "N",
+                    "prdCustTypCd": "A01",
+                    "fstScbDcYn": "Y",
+                    "fstScbDcPrc": "100",
+                    "plusDcYn": "N",
+                    "plusDcPrc": null,
+                    "stkYn": "Y"
+                },
+                {
+                    "prdId": "PR00000228",
+                    "prdNm": "파리바게뜨 할인",
+                    "prdBnfNm": "파리바게뜨 1,000원당 300원 할인",
+                    "prdImg": "FOBO/PRD/PRDDSP/20240110/thumb_파리바게트.png",
+                    "prdFprcAmt": "0",
+                    "prdSprcAmt": "7900",
+                    "discntRate": "0",
+                    "scbPayTrmCd": "03",
+                    "sllStCd": "02",
+                    "likeYn": "N",
+                    "prdCustTypCd": "B02",
+                    "fstScbDcYn": "Y",
+                    "fstScbDcPrc": "1000",
+                    "plusDcYn": "N",
+                    "plusDcPrc": null,
+                    "stkYn": "Y"
+                },
+                {
+                    "prdId": "PR00000344",
+                    "prdNm": "CU 할인",
+                    "prdBnfNm": "CU 매장 구매 1,000원당 300원 할인",
+                    "prdImg": "FOBO/PRD/PRDDSP/20240509/thumb_CU_240422-fQLQdqHUgeyAyj7.png",
+                    "prdFprcAmt": "0",
+                    "prdSprcAmt": "4900",
+                    "discntRate": "0",
+                    "scbPayTrmCd": "03",
+                    "sllStCd": "02",
+                    "likeYn": "N",
+                    "prdCustTypCd": "A01",
+                    "fstScbDcYn": "N",
+                    "fstScbDcPrc": null,
+                    "plusDcYn": "Y",
+                    "plusDcPrc": "1800",
+                    "stkYn": "Y"
+                }
+            ]
+        }
+    }
